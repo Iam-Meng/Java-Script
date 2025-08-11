52edf136a78c759f216d01424cfbd2940828bbaa
Iam-Meng <pochseavmeng@gmail.com>
2025-08-11 11:06:54 +0700

    w2.js

---
+++ w2.js
@@ -0,0 +1,17 @@
+// Ex 1
+
+// function printHeart(){
+//     console.log("<3");
+// }
+
+// printHeart();
+
+// Ex 2
+
+function rant(message){
+    for(let i = 0; i < 3; i++){
+        console.log(message.toUpperCase());
+    }
+}
+
+rant("i hate beets");
