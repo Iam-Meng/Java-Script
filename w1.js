402e6545ea1e7359f9cfd81a2449da2d76c1f148
Iam-Meng <pochseavmeng@gmail.com>
2025-08-11 11:08:40 +0700

    new file:   w1.js

---
+++ w1.js
@@ -0,0 +1,84 @@
+// Ex 1
+
+// let firstName = "Bro";
+// let lastName = "Pav Rouk Jit";
+// console.log("Hello, My name is",firstName,lastName);
+
+// Ex 2
+
+// let a = 10; let b = 5; let c = 2;
+// console.log(a * b + c);
+
+// Ex 3
+
+// let isSunny = true;
+// if(isSunny){
+//     console.log("Wear sunglasses");
+// }else{
+//     console.log("Take an umbrella");
+// }
+
+// Ex 4
+
+// let arr = [10,20,30,40,50];
+// arr.forEach(arr =>{
+//     if (arr > 10){
+//         console.log(arr);
+//     }
+// });
+
+// Ex 5
+
+// let book = { 
+//     title: "JavaScript Basics",
+//     author: "sreyKal",
+//     pages: 120
+// };
+// console.log(`The book ${book.title} by ${book.author} has ${book.pages} pages`);
+
+// Ex 6
+
+// let num = 1;
+// while(num <= 20){
+//     if (num % 2 == 0){
+//         console.log(num);
+//     }
+//     num++;
+// }
+
+// Ex 7
+
+// let num = 10;
+// while(num > 0){
+//     console.log(num);
+//     num--;
+// }
+
+// Ex 8
+
+// let arr = [1,2,3,4,5];
+// arr.forEach(arr=>{
+//     console.log(arr*2);
+// });
+
+// Ex 9
+
+// let str = "JavaScript is fun!";
+// let text = "";
+// for(let x of str){
+//     if (x != " "){
+//         text += x;
+//     }
+// }
+// console.log(text.toUpperCase());
+
+// Ex 10
+
+// let score = [50,67,60,78,98,100];
+// let count = 0;
+// score.forEach(score=>{
+//     if(score >= 60){
+//         count++;
+//     }
+// });
+// console.log(count);
